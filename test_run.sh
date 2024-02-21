da8eecd24b32dc14a885df1f99cb7a19708f14ce
CourcelleM <44867828+CourcelleM@users.noreply.github.com>
2024-02-21 12:19:28 +0100

    specified snakemake version

--- test_run.sh
+++ test_run.sh
@@ -2,7 +2,7 @@
 # Simple wrapper to run a snakemake dry-run with current parameters;
 
 if module list -t 2>&1 | grep -q snakemake ; then loaded="yes" ; else loaded="no" ; fi
-module load snakemake
+module load snakemake/7.32.4-conda
 
 snakemake -s scripts/Map_to_reference.snake --configfile=scripts/config.yaml --dry-run --reason
 
